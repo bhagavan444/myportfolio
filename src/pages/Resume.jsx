2d23d7576189f0d4122d30be502700336d14a8e5
bhagavan444 <g.sivasatyasaibhagavan@gmail.com>
2025-07-10 13:01:01 +0530

    final update32

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import resumePDF from "../assets/updated.pdf";
+import resumePDF from "../assets/bhagavanresume1page.pdf";
 import "../styles/Resume.css";
 
 const Resume = () => {
